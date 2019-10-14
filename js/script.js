5e5880fb785eecf022707aecaaf1ba23a50d340b
IhorFedorov7 <ihor.fedorov.life@gmail.com>
2019-10-14 22:03:36 +0300

    Создание слфйдера и калькулятора так же были созданы формы

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //Timer
 
-    let deadline = '2019-10-06';
+    let deadline = '2019-10-07';
 
     function getTimeRemining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -85,4 +85,186 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
     setClock('timer', deadline);
+
+    //modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        btmDescription = document.querySelectorAll('.description-btn');
+
+    function moreBtn() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    };
+
+    function closeBtn() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    }
+
+    more.addEventListener('click', moreBtn);
+    close.addEventListener('click', closeBtn);
+
+    btmDescription.forEach((items, i) => {
+        items.addEventListener('click', moreBtn)
+    })
+
+    //Form
+
+    let massage = {
+        loading: 'Загрузка...',
+        success: 'Спасибо скоро мы с Вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    }
+
+    let form = document.querySelector('.main-form'),
+        formF = document.querySelector('#form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    function sendF(elem) {
+
+        elem.addEventListener('submit', function(event) {
+            event.preventDefault();
+            elem.appendChild(statusMessage);
+
+            let formData = new FormData(elem);
+    
+            function postD() {
+                return new Promise(function(resolve, reject) {
+                    let requst = new XMLHttpRequest();
+                    requst.open('POST', 'server.php');
+                    requst.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            
+                    requst.onreadystatechange = function() {
+                        if(requst.readyState < 4) {
+                            statusMessage.innerHTML = massage.length;
+                        } else if (requst.readyState === 4) {
+                            if (requst.status === 200 && requst.status < 300) {
+                                resolve()
+                            } else {
+                                reject()
+                            }
+                        }
+                    };
+                    requst.send(formData);
+                })
+            }
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+            }
+            postD(formData)
+                .then(() =>statusMessage.innerHTML =  massage.success)
+                .catch(() => statusMessage.innerHTML = massage.failure)
+                .then(clearInput)
+        });
+    } 
+    sendF(form);
+    sendF(formF);
+
+    //Slider
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    } 
+    
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlide(i);
+            }
+        }
+    });
+
+    //Calc
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        totalValue.innerHTML = 0;
+        persons.addEventListener('change', function() {
+            personsSum = +this.value;
+            total = (daysSum + personsSum) * 4000;
+
+            if (restDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+        });
+
+        restDays.addEventListener('change', function() {
+            daysSum = +this.value;
+            total = (daysSum + personsSum) * 4000;
+
+            if (restDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+        });
+
+        place.addEventListener('change', function() {
+            if (restDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+        })
 });
+
+
+
+
+
